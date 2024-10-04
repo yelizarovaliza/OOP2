70148a61861a2714b62f28e8f3bbb252951e8ca3
yelizarovaliza <yyelizarova@kse.org.ua>
2024-10-05 00:11:13 +0300

    add board and shapes command

--- Console2/Console2.cpp
+++ Console2/Console2.cpp
@@ -28,8 +28,14 @@ struct Board {
             grid[y][x] = c;
         }
     }
+
+    // Method to clear the board
+    void clear() {
+        grid.assign(BOARD_HEIGHT, vector<char>(BOARD_WIDTH, ' '));
+    }
 };
 
+// Abstract base class Shape
 class Shape {
 public:
     virtual void draw(Board& board) = 0;
@@ -72,13 +78,14 @@ public:
     }
 };
 
-// RightTriangle class derived from Shape
+// Triangle class derived from Shape
 class Triangle : public Shape {
     int x, y, length;
 
 public:
     Triangle(int left, int top, int l) : x(left), y(top), length(l) {}
 
+    // Draw a right triangle
     void drawRight(Board& board) {
         for (int i = 0; i < length; ++i) {
             for (int j = 0; j <= i; ++j) {
@@ -88,31 +95,50 @@ public:
             }
         }
     }
-    void drawEquilateral(Board& board) {
+
+    // Draw only the frame of an equilateral triangle
+    void drawEquilateralFrame(Board& board) {
+        // Draw left edge of the triangle
         for (int i = 0; i < length; ++i) {
             board.setPixel(x - i, y + i, '*');
         }
+
+        // Draw right edge of the triangle
         for (int i = 0; i < length; ++i) {
             board.setPixel(x + i, y + i, '*');
         }
+
+        // Draw the base of the triangle
         for (int j = x - (length - 1); j <= x + (length - 1); ++j) {
             board.setPixel(j, y + (length - 1), '*');
         }
     }
+
     void draw(Board& board, const string& type) {
         if (type == "right") {
             drawRight(board);
         }
         else if (type == "equal") {
-            drawEquilateral(board);
+            drawEquilateralFrame(board);
         }
     }
+
     void draw(Board& board) override {
-        drawEquilateral(board);
+        drawEquilateralFrame(board);
     }
 };
+
+// Command class to handle shapes
 class Commands {
+    vector<Shape*> shapes;  // Store shapes added to the board
+
 public:
+    ~Commands() {
+        for (Shape* shape : shapes) {
+            delete shape;  // Free dynamically allocated memory
+        }
+    }
+
     void addShape(const string& command, Board& board) {
         istringstream stream(command);
         string action, shapeType, triangleType;
@@ -122,26 +148,45 @@ public:
             if (shapeType == "circle") {
                 int x, y, radius;
                 stream >> x >> y >> radius;
-                Circle circle(x, y, radius);
-                circle.draw(board);
+                Circle* circle = new Circle(x, y, radius);
+                shapes.push_back(circle);
+                circle->draw(board);
             }
             else if (shapeType == "rectangle") {
                 int x, y, width, height;
                 stream >> x >> y >> width >> height;
-                Rectangle rectangle(x, y, width, height);
-                rectangle.draw(board);
+                Rectangle* rectangle = new Rectangle(x, y, width, height);
+                shapes.push_back(rectangle);
+                rectangle->draw(board);
             }
             else if (shapeType == "triangle") {
                 stream >> triangleType;
                 int x, y, length;
                 stream >> x >> y >> length;
-                Triangle triangle(x, y, length);
-                triangle.draw(board, triangleType);
+                Triangle* triangle = new Triangle(x, y, length);
+                shapes.push_back(triangle);
+                triangle->draw(board, triangleType);
             }
         }
     }
-};
 
+    // Draws all shapes on the board
+    void drawAllShapes(Board& board) {
+        board.clear();
+        for (Shape* shape : shapes) {
+            shape->draw(board);
+        }
+    }
+
+    // Lists the available shape commands and their formats
+    void listShapes() {
+        cout << "Available shapes and parameters:\n";
+        cout << "1. Circle: add circle <centerX> <centerY> <radius>\n";
+        cout << "2. Rectangle: add rectangle <leftX> <topY> <width> <height>\n";
+        cout << "3. Triangle (Right): add triangle right <leftX> <topY> <length>\n";
+        cout << "4. Triangle (Equilateral): add triangle equal <centerX> <topY> <length>\n";
+    }
+};
 
 int main() {
     Board board;
@@ -154,8 +199,17 @@ int main() {
         if (command == "exit") {
             break;
         }
-        c.addShape(command, board);
-        board.print();
+        else if (command == "draw") {
+            c.drawAllShapes(board);
+            board.print();
+        }
+        else if (command == "shapes") {
+            c.listShapes();
+        }
+        else {
+            c.addShape(command, board);
+            board.print();
+        }
         cout << "\n";
     }
 
